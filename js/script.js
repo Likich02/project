dfac9ef65885d727c5ff09d8a1ffd448d69702ef
Danil <example@gmail.com>
2023-10-11 17:40:51 +0700

    first commit

---
+++ js/script.js
@@ -0,0 +1,17 @@
+let incr = 10,
+  decr = 10;
+
+// incr++;
+// decr--;
+
+console.log(++incr);
+console.log(--decr);
+
+console.log(5 % 3);
+
+console.log(2 * 4 === "8");
+
+const isChecked = true,
+  isClose = false;
+
+console.log(isChecked || isClose);
