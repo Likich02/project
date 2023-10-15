d1620d8c4f0cf4cd6ceff65fbfcf96bc086e3936
Danil <example@gmail.com>
2023-10-15 22:45:46 +0700

    script.js  mod

--- js/script.js
+++ js/script.js
@@ -8,12 +8,26 @@ const personalMomieDB = {
   privat: false,
 };
 
-const a = prompt("Один из послендних просмотренных фильмов?", ""),
-  b = prompt("На сколько оцените его?", ""),
-  c = prompt("Один из послендних просмотренных фильмов?", ""),
-  d = prompt("На сколько оцените его?", "");
+for (let i = 0; i < 2; i++) {
+  const a = prompt("Один из послендних просмотренных фильмов?", ""),
+    b = prompt("На сколько оцените его?", "");
+  if (a != null && b != null && a != "" && b != "" && a.length < 50) {
+    personalMomieDB.movies[a] = b;
+    console.log("done");
+  } else {
+    console.log("Error");
+    i--;
+  }
+}
 
-personalMomieDB.movies[a] = b;
-personalMomieDB.movies[c] = d;
+if (personalMomieDB.count < 10) {
+  console.log("Просмотренно мало фильмов");
+} else if (personalMomieDB.count >= 10 && personalMomieDB.count < 30) {
+  console.log("Вы классический зритель");
+} else if (personalMomieDB.count >= 30) {
+  console.log("Вы киноман");
+} else {
+  console.log("Ошибка");
+}
 
 console.log(personalMomieDB);
