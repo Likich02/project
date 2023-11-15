057a963b4a357cf6b8f6496088d7c742183db8cc
Danil <example@gmail.com>
2023-11-15 18:37:56 +0700

    script.js  mod

--- js/script.js
+++ js/script.js
@@ -1,81 +1,80 @@
-let incr = 10,
-  decr = 10;
-let numberOfFilms;
-
-function start() {
-  numberOfFilms = +prompt("Сколько фильмов вы уже посмотрели?", "");
-  while (numberOfFilms == "" || numberOfFilms == null || isNaN(numberOfFilms)) {
-    numberOfFilms = +prompt("Сколько фильмов вы уже посмотрели?", "");
-  }
-}
-start();
 const personalMomieDB = {
-  count: numberOfFilms,
+  count: 0,
   movies: {},
   actors: {},
   geners: [],
   privat: false,
-};
-
-function rememberMyFilms() {
-  for (let i = 0; i < 2; i++) {
-    const a = prompt("Один из послендних просмотренных фильмов?", ""),
-      b = prompt("На сколько оцените его?", "");
-    if (a != null && b != null && a != "" && b != "" && a.length < 50) {
-      personalMomieDB.movies[a] = b;
-      console.log("done");
-    } else {
-      console.log("eror");
-      i--;
+  start: function () {
+    personalMomieDB.count = +prompt("Сколько фильмов вы уже посмотрели?", "");
+    while (
+      personalMomieDB.count == "" ||
+      personalMomieDB.count == null ||
+      isNaN(personalMomieDB.count)
+    ) {
+      personalMomieDB.count = +prompt("Сколько фильмов вы уже посмотрели?", "");
     }
-  }
-}
-rememberMyFilms();
+  },
+  rememberMyFilms: function () {
+    for (let i = 0; i < 2; i++) {
+      const a = prompt("Один из послендних просмотренных фильмов?", ""),
+        b = prompt("На сколько оцените его?", "");
+      if (a != null && b != null && a != "" && b != "" && a.length < 50) {
+        personalMomieDB.movies[a] = b;
+        console.log("done");
+      } else {
+        console.log("eror");
+        i--;
+      }
+    }
+  },
+  detectPersonalLevel: function () {
+    if (personalMomieDB.count < 10) {
+      console.log("Просмотрено довольно мало фильмов");
+    } else if (personalMomieDB.count >= 10 && personalMomieDB.count < 30) {
+      console.log("Вы классический зритель");
+    } else if (personalMomieDB.count >= 30) {
+      console.log("Вы киноман");
+    } else {
+      console.log("Произошла ошибка");
+    }
+  },
+  showMyDB: function (hidden) {
+    if (!hidden) {
+      console.log(personalMomieDB);
+    }
+  },
 
-function detectPersonalLevel() {
-  if (personalMomieDB.count < 10) {
-    console.log("Просмотрено довольно мало фильмов");
-  } else if (personalMomieDB.count >= 10 && personalMomieDB.count < 30) {
-    console.log("Вы классический зритель");
-  } else if (personalMomieDB.count >= 30) {
-    console.log("Вы киноман");
-  } else {
-    console.log("Произошла ошибка");
-  }
-}
+  toggleVisibleMyDB: function () {
+    if (personalMomieDB.privat) {
+      personalMomieDB.privat = false;
+    } else {
+      personalMomieDB.privat = true;
+    }
+  },
 
-detectPersonalLevel();
+  writeYouGeners: function () {
+    for (let i = 1; i < 2; i++) {
+      // let genre = prompt(`Ваш любимый жанр под номером ${i}`);
 
-function showMyDB(hidden) {
-  if (!hidden) {
-    console.log(personalMomieDB);
-    12;
-  }
-}
-showMyDB();
+      // if (genre === "" || genre == null) {
+      //   console.log("Вы ввели некорректные данные");
+      //   i--;
+      // } else {
+      //   personalMomieDB.geners[i - 1] = genre;
+      // }
+      let genres = prompt(`Введите ваши любимые жанры через запятую ${i}`);
 
-function writeYouGeners() {
-  for (let i = 1; i <= 3; i++) {
-    personalMomieDB.geners[i - 1] = promt(`Ваш любимый жанр под номером ${i}`);
-  }
-}
+      if (genres === "" || genres == null) {
+        console.log("Вы ввели некорректные данные");
+        i--;
+      } else {
+        personalMomieDB.geners = genres.split(", ");
+        personalMomieDB.geners.sort();
+      }
+    }
 
-writeYouGeners();
-
-// console.log(++incr);
-// console.log(--decr);
-
-// // c = prompt("Один из послендних просмотренных фильмов?", ""),
-// // d = prompt("На сколько оцените его?", "");
-
-// console.log(5 % 3);
-// personalMomieDB.movies[a] = b;
-// personalMomieDB.movies[c] = d;
-
-// console.log(2 * 4 === "8");
-
-// const isChecked = true,
-//   isClose = false;
-
-// console.log(isChecked || isClose);
-console.log(personalMomieDB);
+    personalMomieDB.geners.forEach((item, i) => {
+      console.log(`Любимый жанр ${i + 1} - это ${item} `);
+    });
+  },
+};
